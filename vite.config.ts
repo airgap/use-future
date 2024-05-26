9041cde99b82d0ed8de00a610ccab6f9169eeb1c
Nicole Muzzin <nicole@muzz.in>
2024-05-26 00:19:35 -0400

    Fix tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
 import { defineConfig } from "vitest/config";
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Use Future',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        }
+      }
+    }
+  },
+  plugins: [dts()],
   test: {
     setupFiles: ["./setupTests.ts"],
   },
