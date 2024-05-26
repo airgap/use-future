8197603fc6b3601d366a816121adda229319ddf5
Nicole Muzzin <nicole@muzz.in>
2024-05-26 01:04:14 -0400

    Formatting

--- src/useFuture.ts
+++ src/useFuture.ts
@@ -1,45 +1,33 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, DependencyList } from "react";
 
 export const useFuture = <T>(
   promiseFactory: () => Promise<T>,
-  deps: unknown[] = [],
-): [T | undefined, unknown | undefined, boolean] => {
+  deps: DependencyList = [],
+): readonly [T | undefined, unknown | undefined, boolean] => {
   const [value, setValue] = useState<T | undefined>(undefined);
   const [error, setError] = useState<unknown | undefined>(undefined);
   const [loading, setLoading] = useState(true);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const memoizedPromiseFactory = useCallback(promiseFactory, deps);
 
   useEffect(() => {
     let isMounted = true;
 
     const fetchData = async () => {
-      let promise: Promise<T> | undefined;
       try {
-        promise = memoizedPromiseFactory();
+        const promise = memoizedPromiseFactory();
+        const resolvedValue = await promise;
+        if (isMounted) {
+          setValue(resolvedValue);
+          setError(undefined);
+          setLoading(false);
+        }
       } catch (err: unknown) {
         if (isMounted) {
           setValue(undefined);
           setError(err);
           setLoading(false);
         }
-        return;
-      }
-
-      try {
-        const resolvedValue: T = await promise;
-        if (isMounted) {
-          setValue(resolvedValue);
-          setError(undefined);
-        }
-      } catch (err: unknown) {
-        if (isMounted) {
-          setValue(undefined);
-          setError(err);
-        }
-      } finally {
-        setLoading(false);
       }
     };
 
@@ -50,5 +38,5 @@ export const useFuture = <T>(
     };
   }, [memoizedPromiseFactory]);
 
-  return [value, error, loading];
+  return [value, error, loading] as const;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
 import { defineConfig } from "vitest/config";
-import { resolve } from 'path';
-import dts from 'vite-plugin-dts';
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'Use Future',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "Use Future",
+      fileName: "index.js",
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        }
-      }
-    }
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
   },
   plugins: [dts()],
   test: {
